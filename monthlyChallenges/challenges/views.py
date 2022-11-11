cb6fa3aec3ee0aab7f00b9afdb88f18e6b05f711
Samara Caldas <92318337+samaracaldas@users.noreply.github.com>
2022-11-11 15:54:26 -0300

    Add files via upload

--- monthlyChallenges/challenges/views.py
+++ monthlyChallenges/challenges/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.http import Http404, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
+
+
 
 
 monthly_challenges = {
@@ -15,25 +17,19 @@ monthly_challenges = {
     "september": "Learn Django for at least 20",
     "october": "Eat no meat for the entire month!",
     "november" : "Walk at least 20 minutes  every day.",
-    "december": "Learn Django for at least 20 minutes every day."     
+    "december": None     
         
 }
-
 
 # Create your views here.
 
 def index(request):
-    list_items = ""
     months = list(monthly_challenges.keys())
     
-    for month in months:
-        capitalized_month = month.capitalize()  
-        month_path = reverse("month-challenge", args=[month])  
-        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>" 
-
-    # "<li><a href="...">January</a></li><li><a href="...">February</a></li>..."s 
-    response_data = f"<ul>{list_items}</ul>"
-    return HttpResponse(response_data)
+    return render(request, "challenges/index.html", {
+        "months" : months
+    })
+        
 
 def monthly_challenge_by_number(request, month):
     months = list(monthly_challenges.keys())
@@ -49,9 +45,13 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        response_data = f"<h1>{challenge_text}</h1>"
-        return HttpResponse(response_data) 
+        
+        return render(request, "challenges/challenge.html", {
+            "text": challenge_text,
+            "month_name": month.capitalize()
+            
+        } )
     except:
-        return HttpResponse(f"<h1>This month is not supported!</h1>")
+        raise Http404()
 
 
